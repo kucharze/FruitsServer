642b5dbbff36bf3ad4a08eafecfad48d5b5543ef
Zachary Kuchar <zacharyk323@live.com>
2023-08-08 14:10:34 -0400

    Indicvidual fruits and updated array

--- index.js
+++ index.js
@@ -2,13 +2,33 @@ const express = require("express");
 
 const app = express();
 
-const fruits = ["Appple", "Bannna", "pear"];
+const fruits = [
+  {
+    name: "apple",
+    color: "red",
+    readyToEat: true,
+  },
+  {
+    name: "pear",
+    color: "green",
+    readyToEat: false,
+  },
+  {
+    name: "banana",
+    color: "yellow",
+    readyToEat: true,
+  },
+];
 
 //Routes
 
 //Index all fruits
 app.get("/fruits", (req, res) => {
   res.send(fruits);
+});
+
+app.get("/fruits/:indexOfFruitsArray", (req, res) => {
+  res.send(fruits[req.params.indexOfFruitsArray]);
 });
 
 app.listen("3000", (req, res) => {
