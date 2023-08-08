1ae670e391b3bbdd65dc5e69804c57e13dd25517
Zachary Kuchar <zacharyk323@live.com>
2023-08-08 14:00:49 -0400

    Initial route

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const express = require("express");
 
 const app = express();
 
+const fruits = ["Appple", "Bannna", "pear"];
+
+//Routes
+
+//Index all fruits
+app.get("/fruits", (req, res) => {
+  res.send(fruits);
+});
+
 app.listen("3000", (req, res) => {
   console.log(`Server now listening on port 3000`);
 });
