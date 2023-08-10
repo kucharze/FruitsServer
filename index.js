d0e036c3ea1e95ce99939e6c4410647f541d0308
Zachary Kuchar <zacharyk323@live.com>
2023-08-10 11:03:13 -0400

    Added new paths and components

--- index.js
+++ index.js
@@ -8,11 +8,37 @@ const fruits = require("./models/fruits");
 app.set("view engine", "jsx");
 app.engine("jsx", require("express-react-views").createEngine());
 
+//Tell express to use middleware
+//Encode the url to read in a acertain way
+app.use(express.urlencoded({ extended: false }));
+
 //Routes
+app.use((req, res, next) => {
+  console.log("I run for all routes");
+  next(); //allows the next necessary route to run
+});
 
 //Index all fruits
 app.get("/fruits", (req, res) => {
   res.render("Index", { fruitList: fruits });
+});
+
+//put this above your Show route
+app.get("/fruits/new", (req, res) => {
+  res.render("New");
+});
+
+app.post("/fruits", (req, res) => {
+  if (req.body.readyToEat === "on") {
+    //if checked, req.body.readyToEat is set to 'on'
+    req.body.readyToEat = true; //do some data correction
+  } else {
+    //if not checked, req.body.readyToEat is undefined
+    req.body.readyToEat = false; //do some data correction
+  }
+  fruits.push(req.body);
+  console.log(fruits);
+  res.send("data received");
 });
 
 app.get("/fruits/:indexOfFruitsArray", (req, res) => {
